28b837e12c37d2c6b4621d4017c0c98ce2386d70
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-09-16 22:19:11 +0530

    initial commit

---
+++ BottomViewOfBinaryTree.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+        vector<int> ans;
+        map<int,int>mp;
+        queue<pair<Node*,int>>q;
+        if(root==NULL){
+            return ans;
+        }
+        q.push({root,0});
+        while(!q.empty())
+        {
+            auto p=q.front();
+            q.pop();
+            Node*node=p.first;
+            int index=p.second;
+            mp[index]=node->data;
+            if(node->left!=NULL){
+                q.push({node->left,index-1});
+            }
+            if(node->right!=NULL){
+                q.push({node->right,index+1});
+            }
+           
+        }
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
