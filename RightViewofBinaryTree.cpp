166bf5801a8e71bb2c943f713dcf3e0b9f03fd47
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-09-16 22:55:37 +0530

    initial commit

---
+++ RightViewofBinaryTree.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    void revPreorder(Node*root,int level,vector<int>&ans)
+    {
+        if(root==NULL) return;
+        
+        if(level==ans.size())
+         ans.push_back(root->data);
+        revPreorder(root->right,level+1,ans);
+        revPreorder(root->left,level+1,ans);     
+    }
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(Node *root)
+    {
+      vector<int> ans;//storing right view
+       revPreorder(root,0,ans);
+       return ans;
+    }
+};
